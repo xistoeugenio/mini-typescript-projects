d85ea0f3da6787b94788db8252c7f26294481d47
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2022-12-20 14:30:52 -0300

    created react app using typescript template

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import { RouterProvider } from "react-router-dom"
-import { routes } from "./routes";
-import { Container } from "react-bootstrap";
-import Navbar from "./components/Navbar";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-    <Navbar/>
-      <Container className="mb-4">
-        <RouterProvider router={routes} />
-      </Container>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
