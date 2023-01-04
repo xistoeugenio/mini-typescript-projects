2c807e18b2d576a6b5d2227af363d807f351ae33
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2023-01-04 13:41:50 -0300

    created header, main and footer in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import './app.scss'
+import { typescriptProjects } from './data';
 
 function App() {
   return (
     <div className="App">
+      <header className='appHeader'>
+        <span>this is a project to contain small projects from Xisto Eugenio</span>
+        <h1>Mini-Projects</h1>
+      </header>
+      <main className='appMain'>
+        <div className="projectsContainer">
+          <h3>typescript</h3>
+          {typescriptProjects.map(project =>
+            <div>{project.name}</div>
+          )}
+        </div>
+      </main>
+      <footer className='appFooter'>this is footer</footer>
     </div>
   );
 }
---
+++ src/data.ts
@@ -0,0 +1,11 @@
+export const typescriptProjects = [{
+    name: "type",
+    image: "fkfkdjg",
+    description: "there is nothing here, dont you worry"
+},
+{
+    name: "another",
+    image: "fkfkdjg",
+    description: "there is nothing here, just  another project"
+}
+]
