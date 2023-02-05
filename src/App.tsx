d62cf9e305a71baa45657bc85007441092753fc4
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2023-02-05 15:50:32 -0300

    react already has access to firebase photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
 import * as C from './app.styles'
+import * as Photos from './services/photos';
+import { Photo } from './types/Photos'
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState<Photo[]>([])
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll())
+      setLoading(false);
+    }
+    getPhotos()
+  }, [])
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Photos gallery</C.Header>
+
+        {loading ? "loading" :
+          <C.PhotoList>
+            {photos?.map((item, i) => (
+              <div>{item.name}</div>
+            ))}
+          </C.PhotoList>}
       </C.Area>
     </C.Container>
   );
--- src/app.styles.ts
+++ src/app.styles.ts
@@ -4,17 +4,23 @@ export const Container = styled.div`
     background-color: #27282F;
     color: #FFF;
     min-height: 100vh;
-`
+`;
 
 export const Area = styled.div`
     margin: auto;
     max-width: 980px;
     padding: 30 0;
-`
+`;
 
 export const Header = styled.h1`
     margin: 0;
     padding: 0;
     text-align: center;
     margin-bottom: 30px;
-`
+`;
+
+export const PhotoList = styled.div`
+    display: grid;
+    grid-template-colums: repeat(4, 1fr);
+    gap: 10px;
+`;
