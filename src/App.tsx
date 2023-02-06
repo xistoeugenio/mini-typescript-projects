582603a63f0e8fb9d97aada9c88506129e95bf2c
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2023-02-06 13:41:03 -0300

    'insert' function created to upload the images to firebase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, FormEvent } from 'react';
 import * as C from './app.styles'
 import * as Photos from './services/photos';
 import { Photo } from './types/Photos'
+import { PhotoItem } from './components/photoItem'
 
 function App() {
+  const [uploading, setUploading] = useState(false)
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState<Photo[]>([])
 
@@ -16,15 +18,32 @@ function App() {
     getPhotos()
   }, [])
 
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+
+    if(file && file.size > 0){
+      setUploading(true)
+      setUploading(false)
+    }
+  }
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Photos gallery</C.Header>
 
+        <C.UploadForm method = "POST" onSubmit={handleFormSubmit}>
+          <input type="file" name='image' />
+          <input type="submit" value="Enviar"/>
+        </C.UploadForm>
+
         {loading ? "loading" :
           <C.PhotoList>
             {photos?.map((item, i) => (
-              <div>{item.name}</div>
+              <PhotoItem key={i} url={item.url} name={item.name}/>
             ))}
           </C.PhotoList>}
       </C.Area>
--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,14 +1,15 @@
 import { Photo } from "../types/Photos"
 import { storage } from "../libraries/firebase"
-import { ref, listAll, getDownloadURL } from "firebase/storage"
+import { ref, listAll, getDownloadURL, uploadBytes } from "firebase/storage"
+import { v4 as createId } from 'uuid'
 
 export const getAll = async () => {
     let list: Photo[] = []
 
-    const imagesFolder =ref(storage, "images")
+    const imagesFolder = ref(storage, "images")
     const photoList = await listAll(imagesFolder)
 
-    for(let i in photoList.items){
+    for (let i in photoList.items) {
         let photoUrl = await getDownloadURL(photoList.items[i])
 
         list.push({
@@ -18,4 +19,19 @@ export const getAll = async () => {
     }
 
     return list
+}
+
+export const insert = async (file: File) => {
+    if (['image/jpeg', 'image/jpg', 'image/png'].includes(file.type)) {
+
+        let randomName = createId()
+        let newFile = ref(storage, `images/${randomName}`)
+
+        let upload = await uploadBytes(newFile, file);
+        let photoUrl = await getDownloadURL(upload.ref)
+
+        return { name: upload.ref.name, url: photoUrl } as Photo
+    } else {
+        return new Error('file type not allowed.')
+    }
 }
